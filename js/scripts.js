9e754f75d3e9e65631307930bb829962433e3bd8
Svea Wade <svealinneawade@gmail.com>
2020-11-05 15:35:09 -0800

    Final commit
    co-authored-by Riley McAdoo <riley.mcadoo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,89 @@
-//Business Interface
-let player1 = "Player 1";
-let player2 = "Player 2";
+//Business Logic
 
-function nameInput () {
-  player1 = prompt("Change Player 1 name");
-  player2 = prompt("Change Player 2 name");
+function Player() {
+  this.turnScore = 0;
+  this.scoreTotal = 0;
+  this.scoreTotal2 = 0;
+}
 
-  document.querySelector("p.Player1").innerHTML = player1;
-  document.querySelector("p.Player2").innerHTML = player2;
-} 
-function rollDice () {
-  dice = parseInt(Math.floor(Math.random() *6)) + 1;
-  if (dice > 1) {
-  return dice;
-  } else if (dice = 1 ) {
-    return 0 
+generateNumber = function() {
+  let numberVal = Math.floor( Math.random() *6) + 1;
+  return numberVal;
+}
+
+Player.prototype.rollDice = function() {
+  let roll = generateNumber();
+  if (roll !=1) {
+    return this.turnScore += roll;
+  } else if (roll = 1)  {
+    // $(".rollBtn").toggle();
+    // $(".rollBtn2").toggle();
+    // $(".hold").toggle();
+    // $(".hold2").toggle();
+
+    return this.turnScore *= 0;
+    }
+  }
+//Player 1
+Player.prototype.holdTransfer = function() {
+    if (this.scoreTotal + this.turnScore >= 100)  {
+    alert("PLAYER 1 WINS!");
+  } else {
+    return this.scoreTotal += this.turnScore;
   }
 }
-  
 
-Player.prototype.addRolls = function() rollDice() 
+
 Player.prototype.clearTurnScore = function()  {
   this.turnScore *=0;
   return this.turnScore;
 }
 
-//User Interface
+//Player 2
+
+
+
+
+// User Interface
+
+let turn = new Player; 
+
+//Player 1
 $(document).ready(function() {
-  $("#roll").click(function(){
-    var result = turn.rollDice();
-    $("$p1turn-total").text(result);
+  $(".rollBtn").click(function(){
+  var result = turn.rollDice();
+  $("#p1turn-total").text(result);
   });
   $(".hold").click(function() {
     let result2 = turn.holdTransfer();
     let result3 = turn.clearTurnScore();
-
-    $(".hold").toggle();
-    $(".hold2").toggle();
-    $(".roll").toggle();
-    $(".roll2").toggle();
-
     $("p1score-total").text(result2);
-
     $("p1turn-total").text(result3);
+    // $(".hold").toggle();
+    // $(".hold2").toggle();
+    // $(".rollBtn").toggle();
+    // $(".rollBtn2").toggle();
+    
+
+//Player 2
+    
   });
 
+});
 
 
 
 
 
+// let player1 = "Player 1";
+// let player2 = "Player 2";
+
+// function nameInput () {
+//   player1 = prompt("Change Player 1 name");
+//   player2 = prompt("Change Player 2 name");
+
+//   document.querySelector("p.Player1").innerHTML = player1;
+//   document.querySelector("p.Player2").innerHTML = player2;
+// } 
 
 
-
-
-
-
-
-
-
-
-
-
-
-//document.getElementById("roll").onclick = rollDice();
-
-
-// $(document).ready(function() {
-//   $("#roll").click(function(){
-//     $("#p1turn-total").text(function(){
-//       return dice 
-//     });
-//   });
-//   $("#roll2").click(function(){
-//     $("#p2turn-total").text(function(){
-//       return dice
-//     });
-//   });
-// });
--- js/scripts.test.js
+++ js/scripts.test.js
@@ -1,93 +1,165 @@
 //Business Logic For Dice Roll
 
-function Player() {
-  this.turnScore = 0;
-  this.scoreTotal = 0;
-  this.scoreTotal2 = 0;
-}
 
-generateNumber = function() {
-  var numberVal = Math.floor( Math.random() *6) + 1;
-  return numberVal;
-}
 
-Player.prototype.rollDice = function() {
-  let roll = generateNumber();
-  if (roll !=1) {
-    return this.turnScore += roll;
-  } else if (roll = 1)  {
-    $(".rollBtn").toggle();
-    $(".rollBtn2").toggle();
-    $(".hold").toggle();
-    $(".hold2").toggle();
 
-    return this.turnScore *= 0;
-    }
-  }
-// Player.prototype.holdTransfer = function() {
-//   if (this.scoreTotal + this.turnScore >= 100) {
-//     alert("PLAYER 1 WINS!!!");
-//   } else {
-//     return this.scoreTotal += this.turnScore;
-//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Player() {
+//   this.turnScore = 0;
+//   this.scoreTotal = 0;
+//   this.scoreTotal2 = 0;
 // }
 
-Player.prototype.clearTurnScore = function()  {
-  this.turnScore *=0;
-  return this.turnScore;
-}
-// player2
-Player.prototype.holdTransfer2 = function() {
-  if (this.scoreTotal2 + this.turnScore >= 100)  {
-    alert("PLAYER 2 WINS!");
-  }
-  return this.scoreTotal2 += this.turnScore;
- }
+// generateNumber = function() {
+//   var numberVal = Math.floor( Math.random() *6) + 1;
+//   return numberVal;
+// }
 
-Player.prototype.clearTurnScore2 = function() {
- this.turnScore *=0;
- return this.turnScore;
-}
+// Player.prototype.rollDice = function() {
+//   let roll = generateNumber();
+//   if (roll !=1) {
+//     return this.turnScore += roll;
+//   } else if (roll = 1)  {
+//     $(".rollBtn").toggle();
+//     $(".rollBtn2").toggle();
+//     $(".hold").toggle();
+//     $(".hold2").toggle();
+
+//     return this.turnScore *= 0;
+//     }
+//   }
+// // Player.prototype.holdTransfer = function() {
+// //   if (this.scoreTotal + this.turnScore >= 100) {
+// //     alert("PLAYER 1 WINS!!!");
+// //   } else {
+// //     return this.scoreTotal += this.turnScore;
+// //   }
+// // }
+
+// Player.prototype.clearTurnScore = function()  {
+//   this.turnScore *=0;
+//   return this.turnScore;
+// }
+// // player2
+// Player.prototype.holdTransfer2 = function() {
+//   if (this.scoreTotal2 + this.turnScore >= 100)  {
+//     alert("PLAYER 2 WINS!");
+//   }
+//   return this.scoreTotal2 += this.turnScore;
+//  }
+
+// Player.prototype.clearTurnScore2 = function() {
+//  this.turnScore *=0;
+//  return this.turnScore;
+// }
 
 
-// User Interface
+// // User Interface
 
-let turn = new Player; 
+// let turn = new Player; 
 
-//player 1
-$(document).ready(function() {
-  $(".rollBtn").click(function(){
-    var result = turn.rollDice();
-    $("#p1turn-total").text(result);
-  });
-  $(".hold").click(function() {
-    let result2 = turn.holdTransfer();
-    let result3 = turn.clearTurnScore();
+// //player 1
+// $(document).ready(function() {
+//   $(".rollBtn").click(function(){
+//     var result = turn.rollDice();
+//     $("#p1turn-total").text(result);
+//   });
+//   $(".hold").click(function() {
+//     let result2 = turn.holdTransfer();
+//     let result3 = turn.clearTurnScore();
 
-    $(".hold").toggle();
-    $(".hold2").toggle();
-    $(".rollBtn").toggle();
-    $(".rollBtn2").toggle();
+//     $(".hold").toggle();
+//     $(".hold2").toggle();
+//     $(".rollBtn").toggle();
+//     $(".rollBtn2").toggle();
 
-    $("p1score-total").text(result2);
+//     $("p1score-total").text(result2);
 
-    $("p1turn-total").text(result3);
-  });
+//     $("p1turn-total").text(result3);
+//   });
 
-//player 2
-$(".rollBtn2").click(function() {
-  var result = turn.diceRoll();
-  $("#p2turn-total").text(result);
-});
-$(".hold2").click(function(){
-  var result2 = turn.holdTransfer2();
-  var result3 = turn.clearTurnScore2();
-  $("#p2score-total").text(result2);
-  $("#p2turn-total").text(result3);
-  $(".hold").toggle();
-  $(".hold2").toggle();
-  $(".rollBtn").toggle();
-  $(".rollBtn2").toggle();
-});
+// //player 2
+// // $(".rollBtn2").click(function() {
+// //   var result = turn.diceRoll();
+// //   $("#p2turn-total").text(result);
+// // });
+// // $(".hold2").click(function(){
+// //   var result2 = turn.holdTransfer2();
+// //   var result3 = turn.clearTurnScore2();
+// //   $("#p2score-total").text(result2);
+// //   $("#p2turn-total").text(result3);
+// //   $(".hold").toggle();
+// //   $(".hold2").toggle();
+// //   $(".rollBtn").toggle();
+// //   $(".rollBtn2").toggle();
+// // });
 
-});
+// });
